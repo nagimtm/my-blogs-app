f256dce08d2fa811339854ff80d16dc967f84d0d
nagim <a.nagim@protonmail.com>
2023-10-30 10:25:04 +0100

    change about text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,9 +11,7 @@ const Footer = () => {
             thoughts and experiences with others. Given the active comment
             sections, they enable people to interact with one another and build
             relationships based on shared interests that are not limited by
-            geographic location. They are an impactful form of information
-            exchange. Essentially, blogs have become a social platform unto
-            themselves and a central part of online community building.
+            geographic location.
           </p>
         </div>
         <div className={classes.col}>
