b71eb9fbdb458f257ad85c1648d6f40df8269f19
nagim <a.nagim@protonmail.com>
2023-10-30 10:20:03 +0100

    Add about section text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,9 +7,13 @@ const Footer = () => {
         <div className={classes.col}>
           <h2>About the App</h2>
           <p>
-            the build using react-scripts will error out with the message Cannot
-            find module: . Make sure this package is installed.. this happens
-            before though. I am still using node 14 as usual
+            Much like social media platforms, blogs allow people to share their
+            thoughts and experiences with others. Given the active comment
+            sections, they enable people to interact with one another and build
+            relationships based on shared interests that are not limited by
+            geographic location. They are an impactful form of information
+            exchange. Essentially, blogs have become a social platform unto
+            themselves and a central part of online community building.
           </p>
         </div>
         <div className={classes.col}>
