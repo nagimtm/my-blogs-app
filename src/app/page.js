4da28fcdb750fb2ea11dcf4ffa050a15862525e4
nagim <a.nagim@protonmail.com>
2023-10-30 07:23:50 +0100

    fix bugs

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Card from "@/components/blogCard/Card";
 
 export async function fetchBlogs() {
   try {
-    const res = await fetch("/api/blog", {
+    const res = await fetch("http://127.0.0.1:3000/api/blog", {
       cache: "no-store",
     });
 
